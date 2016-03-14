b84163ec04ec42f652da3c314f02905cf12761a0
Santosh <santosh.dhakal07@gmail.com>
2016-03-14 14:58:50 +0545

    Added all files to repository

---
+++ structure.c
@@ -0,0 +1,33 @@
+typedef struct {
+	GtkWidget *window;
+	GtkWidget *button1;
+	GtkWidget *button2;
+} Form1;
+
+typedef struct {
+	GtkWidget *window;
+	GtkWidget *label1;
+	GtkWidget *buttonConvert;
+	GtkWidget *button3;
+	GtkWidget *combobox1;
+	GtkWidget *combobox2;
+	GtkWidget *textBox1;
+	GtkWidget *checkBox1;
+} Form2;
+
+typedef struct {
+	GtkWidget *window;
+	GtkWidget *label1;
+	GtkWidget *comboBox1;
+	GtkWidget *comboBox2;
+	GtkWidget *comboBox3;
+	GtkWidget *comboBox4;
+	GtkWidget *comboBox5;
+	GtkWidget *comboBox6;
+	GtkWidget *button1;
+	GtkWidget *button2;
+} Form3;
+
+Form1 form1;
+Form2 form2;
+Form3 form3;
